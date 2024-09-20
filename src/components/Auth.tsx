fbf5250d66937f68854e8b67d0aabde48ed29db3
ali-s3 <kashiknigh99@gmail.com>
2024-09-20 15:09:59 +0500

    09/20/2024

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -7,7 +7,7 @@ import styles from '../styles/Auth.module.scss'
 const inter = Inter({ subsets: ['latin'] })
 const manrope = Manrope({ subsets: ['latin'] })
 
-export default function Header() { 
+export default function Header() {  
   return (
     <div className={styles.button}>
       <a type="button" href="https://discord.com/api/oauth2/authorize?client_id=1001763225018318888&redirect_uri=https%3A%2F%2Fnekocorp.gq%2F&response_type=code&scope=identify"><Image src="/dsc.png" alt="dsc" width="20" height="15" /> Discord</a>
